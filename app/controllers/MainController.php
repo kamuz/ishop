a7fa18b1c3bd08d42e1922614338f3ab77b399d2
Vladimir Kamuz <v.kamuz@gmail.com>
2022-10-13 13:50:49 +0300

    Delete temp code

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,11 +8,5 @@ class MainController extends AppController {
 
 	public function indexAction(){
 		$this->setMeta( App::$app->getProperty('shop_name') . ' - Home page', 'This is test description' );
-		$messages = \R::findAll( 'messages' );
-		$this->set( [ 'messages' => $messages ] );
-	}
-
-	public function viewAction(){
-
 	}
 }
