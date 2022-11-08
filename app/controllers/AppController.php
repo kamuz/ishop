3ac07a698d2ae8bf93e090e5848b8ceeedac2e47
Vladimir Kamuz <v.kamuz@gmail.com>
2022-11-08 21:35:38 +0200

    Get currencies as property

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -3,11 +3,17 @@
 namespace app\controllers;
 
 use ishop\base\Controller;
+use ishop\App;
 use app\models\AppModel;
+use app\widgets\currency\Currency;
 
 class AppController extends Controller {
 	public function __construct( $route ) {
 		parent::__construct( $route );
 		new AppModel();
+		$currencies = Currency::getCurrencies();
+		// debug( $currencies );
+		App::$app->setProperty( 'currency', $currencies );
+		// debug( App::$app->getProperty( 'currency' ) );
 	}
 }
