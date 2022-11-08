e6281575c42d5be677cd1bbe3a5beb4625718433
Vladimir Kamuz <v.kamuz@gmail.com>
2022-11-08 21:47:17 +0200

    Add comments

--- app/widgets/currency/Currency.php
+++ app/widgets/currency/Currency.php
@@ -8,23 +8,28 @@ class Currency {
 	protected $currencies;
 	protected $currency;
 
+	// Set template and run widget
 	public function __construct() {
 		$this->tpl = __DIR__ . '/currency_tpl/currency.php';
 		$this->run();
 	}
 
+	// Run
 	protected function run(){
 		$this->getHtml();
 	}
 
+	// Get currencies as accoc array
 	public static function getCurrencies() {
 		return \R::getAssoc( "SELECT code, title, symbol_left, symbol_right, value, base FROM currency ORDER BY base DESC" );
 	}
 
+	// Get current currency
 	public static function getCurrency() {
 
 	}
 
+	// Get HTML
 	protected function getHtml() {
 
 	}
