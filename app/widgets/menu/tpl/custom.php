1f1c257b0030d2aff8ec0467c4fd88f5fd57f2c8
Vladimir Kamuz <v.kamuz@gmail.com>
2022-11-29 18:56:06 +0200

    Edit menu

--- app/widgets/menu/tpl/custom.php
+++ app/widgets/menu/tpl/custom.php
@@ -1,5 +1,5 @@
 <li>
-	<a href="?id=<?php echo $id; ?>"><?php echo $category['title'] ?></a>
+	<a href="category/<?=$category['alias'];?>"><?=$category['title'];?></a>
 	<?php if ( isset( $category['childs'] ) ) : ?>
 		<ul>
 			<?php echo $this->getMenuHtml( $category['childs'] ); ?>
