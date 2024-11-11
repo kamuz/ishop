a6932ebd94dd7f93b4f53482fd7d099d8e838650
Vladimir Kamuz <v.kamuz@gmail.com>
2024-11-11 23:05:06 +0200

    Updated dump.sql

--- dump.sql
+++ dump.sql
@@ -1,13 +1,15 @@
--- Adminer 4.7.8 MySQL dump
+-- Adminer 4.8.1 MySQL 5.6.51 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `ishop`;
 CREATE DATABASE `ishop` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `ishop`;
 
+DROP TABLE IF EXISTS `attribute_group`;
 CREATE TABLE `attribute_group` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -15,6 +17,7 @@ CREATE TABLE `attribute_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `attribute_product`;
 CREATE TABLE `attribute_product` (
   `attr_id` int(10) unsigned NOT NULL,
   `product_id` int(11) unsigned NOT NULL,
@@ -22,6 +25,7 @@ CREATE TABLE `attribute_product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `attribute_value`;
 CREATE TABLE `attribute_value` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `value` varchar(255) NOT NULL,
@@ -32,6 +36,7 @@ CREATE TABLE `attribute_value` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `brand`;
 CREATE TABLE `brand` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -47,6 +52,7 @@ INSERT INTO `brand` (`id`, `title`, `alias`, `img`, `description`) VALUES
 (2,	'Citizen',	'citizen',	'abt-2.jpg',	'Amet sapien eros Integer dolore magna aliqua'),
 (3,	'Royal London',	'royal-london',	'abt-3.jpg',	'Sapien sit amet eros Integer dolore magna aliqua');
 
+DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -58,7 +64,23 @@ CREATE TABLE `category` (
   UNIQUE KEY `alias` (`alias`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `category` (`id`, `title`, `alias`, `parent_id`, `keywords`, `description`) VALUES
+(1,	'Men',	'men',	0,	'Men',	'Men'),
+(2,	'Women',	'women',	0,	'Women',	'Women'),
+(3,	'Kids',	'kids',	0,	'Kids',	'Kids'),
+(4,	'Electronic',	'electronic',	1,	'Electronic',	'Electronic'),
+(5,	'Mechanical',	'mechanical',	1,	'Mechanical',	'Mechanical'),
+(6,	'Casio',	'casio',	4,	'Casio',	'Casio'),
+(7,	'Citizen',	'citizen',	4,	'Citizen',	'Citizen'),
+(8,	'Royal London',	'royal-london',	5,	'Royal London',	'Royal London'),
+(9,	'Seiko',	'seiko',	5,	'Seiko',	'Seiko'),
+(10,	'Epos',	'epos',	5,	'Epos',	'Epos'),
+(11,	'Electronic',	'electronic-11',	2,	'Electronic',	'Electronic'),
+(12,	'Mechanical',	'mechanical-12',	2,	'Mechanical',	'Mechanical'),
+(13,	'Adriatica',	'adriatica',	11,	'Adriatica',	'Adriatica'),
+(14,	'Anne Klein',	'anne-klein',	12,	'Anne Klein',	'Anne Klein');
 
+DROP TABLE IF EXISTS `currency`;
 CREATE TABLE `currency` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
@@ -75,6 +97,7 @@ INSERT INTO `currency` (`id`, `title`, `code`, `symbol_left`, `symbol_right`, `v
 (2,	'dollar',	'USD',	'$',	'',	1.00,	'1'),
 (3,	'euro',	'EUR',	'€',	'',	0.88,	'0');
 
+DROP TABLE IF EXISTS `gallery`;
 CREATE TABLE `gallery` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `product_id` int(10) unsigned NOT NULL,
@@ -83,6 +106,7 @@ CREATE TABLE `gallery` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `modification`;
 CREATE TABLE `modification` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `product_id` int(10) unsigned NOT NULL,
@@ -92,6 +116,7 @@ CREATE TABLE `modification` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -104,6 +129,7 @@ CREATE TABLE `order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `order_product`;
 CREATE TABLE `order_product` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `order_id` int(10) unsigned NOT NULL,
@@ -116,6 +142,7 @@ CREATE TABLE `order_product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_id` tinyint(3) unsigned NOT NULL,
@@ -147,6 +174,7 @@ INSERT INTO `product` (`id`, `category_id`, `brand_id`, `title`, `alias`, `conte
 (9,	6,	4,	'Royal London 20034-02',	'royal-london-20034-02',	'<p>Diam in consequat iaculis, est purus iaculis mauris, imperdiet facilisis ante ligula at nulla. Quisque volutpat nulla risus, id maximus ex aliquet ut. Suspendisse potenti. Nulla varius lectus id turpis dignissim porta. Quisque magna arcu, blandit quis felis vehicula, feugiat gravida diam. Nullam nec turpis ligula. Aliquam quis blandit elit, ac sodales nisl. Aliquam eget dolor eget elit malesuada aliquet. In varius lorem lorem, semper bibendum lectus lobortis ac.</p>\r\n\r\n<p>Mauris placerat vitae lorem gravida viverra. Mauris in fringilla ex. Nulla facilisi. Etiam scelerisque tincidunt quam facilisis lobortis. In malesuada pulvinar neque a consectetur. Nunc aliquam gravida purus, non malesuada sem accumsan in. Morbi vel sodales libero.</p>',	110,	0,	'1',	NULL,	'no_image.jpg',	'0'),
 (10,	6,	4,	'Royal London 41156-02',	'royal-london-41156-02',	'<p>Iaculis, est purus iaculis mauris, imperdiet facilisis ante ligula at nulla. Quisque volutpat nulla risus, id maximus ex aliquet ut. Suspendisse potenti. Nulla varius lectus id turpis dignissim porta. Quisque magna arcu, blandit quis felis vehicula, feugiat gravida diam. Nullam nec turpis ligula. Aliquam quis blandit elit, ac sodales nisl. Aliquam eget dolor eget elit malesuada aliquet. In varius lorem lorem, semper bibendum lectus lobortis ac.</p>\r\n\r\n<p>Mauris placerat vitae lorem gravida viverra. Mauris in fringilla ex. Nulla facilisi. Etiam scelerisque tincidunt quam facilisis lobortis. In malesuada pulvinar neque a consectetur. Nunc aliquam gravida purus, non malesuada sem accumsan in. Morbi vel sodales libero.</p>',	100,	0,	'1',	NULL,	'no_image.jpg',	'0');
 
+DROP TABLE IF EXISTS `related_product`;
 CREATE TABLE `related_product` (
   `product_id` int(10) unsigned NOT NULL,
   `related_id` int(10) unsigned NOT NULL,
@@ -154,6 +182,7 @@ CREATE TABLE `related_product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
@@ -168,4 +197,4 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2022-11-14 09:16:34
+-- 2024-11-11 19:35:59
